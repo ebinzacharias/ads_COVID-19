c2100da5570df570e169c8d55717e73cf7041391
ezachari <ezachari@rhrk.uni-kl.de>
2020-06-11 21:03:28 +0200

    first Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An applied Data Science project on anaylzing the COVID-19 data.',
+    author='Ebin Zacharias',
+    license='MIT',
+)
